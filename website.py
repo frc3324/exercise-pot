163c74819be395d1b029fa5261c317990a93cc16
Seth-MetroBots3324 <55810668+Seth-MetroBots3324@users.noreply.github.com>
2021-02-16 17:18:01 -0500

    Got rid of an unnecessary semicolon

--- website.py
+++ website.py
@@ -24,7 +24,7 @@ INSERT INTO exercises(name, days, duration, timeStart, repeats)
 VALUES ("cool exercise", 3, 60, 1200, 0);
 ''')
 
-exerObj = Exercise("name", ["monday"], 120, 2200, 3);
+exerObj = Exercise("name", ["monday"], 120, 2200, 3)
 print(serialize_data(exerObj))
 
 db.execute('''
